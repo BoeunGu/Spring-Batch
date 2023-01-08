16e65a37e6e8166c242ec3e5fafba3e85a8e90ee
BoeunGu <boeuni@munpia.com>
2023-01-08 20:15:18 +0900

    Spring Batch Project init

--- src/main/java/io/springbatch/springbatchlecture/SpringBatchLectureApplication.java
+++ src/main/java/io/springbatch/springbatchlecture/SpringBatchLectureApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableBatchProcessing
+@EnableBatchProcessing //Spring Batch가 작동하기 위해 선언되어야 하는 애노테이션
 public class SpringBatchLectureApplication {
 
     public static void main(String[] args) {
